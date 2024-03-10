db3119da09ca9d6c05c792c00f1cf9e205d32d34
Jacky Sy <jackysy@apples-iMac.local>
2024-03-11 00:43:22 +0800

    Course 08 Finished

---
+++ course-07-whileloop/Program.cs
@@ -0,0 +1,42 @@
+﻿using System.Formats.Asn1;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int result = 0;
+        int i = 1;
+        while (i <=100)
+        {
+            result +=i;
+            i++;
+        }
+        Console.WriteLine($"i equals to {result}");
+
+
+        Random random = new Random();
+        int digits = random.Next(1,100);
+        //Console.WriteLine(digits);
+        int answer;
+        int guessCount = 1;
+        Console.WriteLine("Guess a number");
+        answer = Convert.ToInt32(Console.ReadLine());
+        while (answer != digits)
+        {
+            if (answer > digits)
+            {
+                Console.WriteLine("Too Large");
+            }
+            else if (answer < digits)
+            {
+                Console.WriteLine("Too small");
+            } 
+            Console.WriteLine("Let's guess again!");
+            answer = Convert.ToInt32(Console.ReadLine());
+            guessCount++;
+        }
+        
+        Console.WriteLine($"You are correct.  The answer is {digits}");
+        Console.WriteLine($"You guessed for {guessCount} times!");
+    }
+}
---
+++ course-08-forloop/Program.cs
@@ -0,0 +1,39 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.WriteLine("Please input any number");
+        int number = Convert.ToInt32(Console.ReadLine());
+        int sum = 0;
+        double product = 1;
+        for (int i = 1; i<=number;i++)
+        {
+            sum+=i;
+            product *=i;
+        }
+        Console.WriteLine($"The sum between 1 and {number} is {sum}");
+        Console.WriteLine($"The product between 1 and {number} is {product}");
+
+        PrintFactor(7);
+        PrintFactor(11);
+        PrintFactor(13);
+        for (int j=1;j<=9;j++)
+        {
+            for (int k=1; k <=9 ; k++)
+            {
+                Console.Write($"{j} x {k} = {j*k}\t");
+            }
+            Console.WriteLine();
+        }
+    }
+
+    public static void PrintFactor(int factor)
+    {
+        int multiply=0;
+        for (int i = 1; multiply < 1000; i++)
+        {
+            multiply = factor * i;
+            Console.WriteLine(multiply);
+        }
+    }
+}
